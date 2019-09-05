65308e56b79b4134d15c1a609c6abd220894735e
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-05 20:32:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
 def reverse_each_word(string)
-  #string = string.split
-  string.each {|string| string.reverse}
+  string = string.split
+  string = string.each {|string| string.reverse}
+  string.join
+  
 end
